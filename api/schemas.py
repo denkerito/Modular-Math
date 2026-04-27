6b57a7721734e0b6a8a2f97025c5c6683a342f7b
Dennis <ismajlidennis@gmail.com>
2026-04-27 15:01:57 +0200

    new schemas for apis

--- api/schemas.py
+++ api/schemas.py
@@ -4,3 +4,11 @@ class Operation(BaseModel):
     op1 : int
     op2 : int
     mod : int = Field(gt = 1)
+
+class UnaryOperation(BaseModel):
+    op1 : int
+    mod : int = Field(gt = 1)
+
+class IntOperation(BaseModel):
+    op1: int
+    op2: int
