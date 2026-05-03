74102d969a32ecb423708fcc49e7354eedb671d4
Dennis <ismajlidennis@gmail.com>
2026-05-03 21:51:11 +0200

    update init

--- src/modMath/__init__.py
+++ src/modMath/__init__.py
@@ -1,11 +1,13 @@
 from .mod_int import ModInt
 from .exceptions import *
 from .cryptography import CaesarCipher, AffineCipher, VigenereCipher
+from .number_theory import chinese_remainder_theorem
 
 __all__ = [
     "ModInt",
     "ModIntException",
     "CaesarCipher",
     "AffineCipher",
-    "VigenereCipher"
+    "VigenereCipher",
+    "chinese_remainder_theorem"
 ]
